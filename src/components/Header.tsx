3b681bfeaa5391375ca869599c8253921922d53b
AssertionBit <113050317+AssertionBit@users.noreply.github.com>
2022-11-25 02:42:28 +0300

    WIP: Added normal header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,22 @@
-import style from "./header.css";
+import "./header.css";
+import { createSignal }from "solid-js"
 
 export default () => {
+    const [navbarToggled, setToggle] = createSignal(false);
+
     return (
-        <nav class={style.nav}>
-            <h1>A?</h1>
+        <nav>
+            <a href="/" class="logo">Zig Language</a>
+            <div class="nav--links">
+                <a href="/docs" className="nav--link">Docs</a>
+                <a href="/docs" className="nav--link">Playground</a>
+                <a href="/docs" className="nav--link">Contributing</a>
+                <a href="/docs" className="nav--link">Tooling</a>
+                <a href="/docs" className="nav--link__primary">Get started</a>
+                <div className="nav--mobile">
+                    <img onClick={() => {setToggle(!navbarToggled)}} class="nav--toggle" src="/static/icons/menu.svg"></img>
+                </div>
+            </div>
         </nav>
     )
 }
